5cb5b09228879375cac8b953958052f94cea435a
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-11-26 10:29:24 +0530

    refactor: comments for the script file

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 import StateManager from './src/stateManager.js'
 
+// once all the HTML, CSS and other assets have loaded, 
+// we start with the JS init.
 window.addEventListener("load", function() {
+
+  // get reference to the canvas object defined in the html page
   const canvas = this.document.getElementById('canvas1');
   const ctx = canvas.getContext("2d");
   canvas.width = 600;
@@ -8,14 +12,14 @@ window.addEventListener("load", function() {
 
   const stateManager = new StateManager(canvas);
 
-
-  // clear rect and paint the whole scene again
   function animate() {
+    // clear rect and paint the whole scene again
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     stateManager.render(ctx);
+    // recursively calls this function to re-draw the updated components
     window.requestAnimationFrame(animate);
   }
 
+  // start the recursive loop for animation
   animate();
-}
-)
+})
