c2c0e24e9e3b41be5a4cf6487cb6a75b48984984
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-12-14 00:11:43 +0700

    udpate: set scoreboard and lives of player

--- src/stateManager.js
+++ src/stateManager.js
@@ -9,18 +9,27 @@ class StateManager {
     this.height = this.canvas.height;
     this.player = new Player(this);
 
+    // game state variables
+    this.playerLives = 3; // starting player with 3 lives
+    this.score = 0;
+    this.gameOver = false;
+
     // Ammunition pool
     this.availableAmmoPool = [];
     this.maxAmmo = 15;
-    this.createAmmoPool();
 
     // Enemy wave control
     this.enemySize = 60;
-    this.enemyRaidGridColumns = 3;
-    this.enemyRaidGridRows = 3;
-
+    this.enemyRaidGridColumns = 2;
+    this.enemyRaidGridRows = 2;
     this.raids = [];
+    this.raidCount = 1;
+    this.newRaidSpawned = false;
+
+
+    // init functions
     this.startEnemyRaid();
+    this.createAmmoPool();
 
     // keyboard event listeners
     this.activeKeys = [];
@@ -60,26 +69,77 @@ class StateManager {
     this.raids.push(new Raid(this));
   }
 
-  checkForAmmoHit(ammo, enemy) {
+  createNewRaid() {
+    this.newRaidSpawned = true;
+    if (Math.random() < 0.5) {
+      this.enemyRaidGridColumns++;
+    } else {
+      this.enemyRaidGridRows++;
+    }
+    this.startEnemyRaid();
+    this.raidCount++;
+
+    // there's a small chance of player life increasing after wave finish
+    if (Math.random() < 0.1) {
+      this.playerLives++;
+    }
+    this.newRaidSpawned = false;
+  }
+
+  // check if ammo has hit enemy or enemy has hit player
+  checkEnemyCollision(enemy, object) {
     // using the 2d collision check for this
     // https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
-    return (ammo.x < enemy.enemyPosX + this.enemySize &&
-      ammo.x + ammo.width > enemy.enemyPosX &&
-      ammo.y < enemy.enemyPosY + this.enemySize &&
-      ammo.y * ammo.height > enemy.enemyPosY
+    return (object.x < enemy.enemyPosX + this.enemySize &&
+      object.x + object.width > enemy.enemyPosX &&
+      object.y < enemy.enemyPosY + this.enemySize &&
+      object.y * object.height > enemy.enemyPosY
     );
   }
 
   render(context) {
     this.player.render(context);
+
     this.player.update();
     this.availableAmmoPool.forEach(ammo => {
       ammo.progress()
       ammo.render(context)
     })
     this.raids.forEach(raid => {
+      if (raid.destroyed) {
+        return;
+      }
       raid.render(context);
+
+      if (raid.enemies.length < 1 && !this.newRaidSpawned && !this.gameOver) {
+        raid.destroyed = true;
+        this.createNewRaid();
+      }
     })
+
+    // check for player's lives
+    if (this.playerLives <= 0) {
+      this.gameOver = true;
+      this.playerLives = 0;
+    }
+
+    // show game stats
+    context.save();
+    context.fillText("Score: " + this.score, 10, 30);
+    context.fillText("Raid: " + this.raidCount, 10, 60);
+    context.fillText("Lives: ", 10, 90);
+    for (let i = 0; i < this.playerLives; i++) {
+      context.fillRect(53 + 10 * (i + 1), 75, 5, 15)
+    }
+
+    if (this.gameOver) {
+      context.textAlign = "center";
+      context.font = "100px Impact";
+      context.fillText("Game Over", this.width * 0.5, this.height * 0.5);
+      context.font = "30px Impact";
+      context.fillText("Press R to restart", this.width * 0.5, this.height * 0.5 + 40);
+    }
+    context.restore();
   }
 }
 
