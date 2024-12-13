de1ca3cde4db0d207f3db3040a1a4a2e08a6b8b8
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-12-14 00:10:54 +0700

    update: add segment for hits and health of enemy

--- src/enemy.js
+++ src/enemy.js
@@ -5,12 +5,14 @@ class Enemy {
     this.enemyPosX = 0;
     this.enemyPosY = 0;
 
+    // enemy hits and health
+    this.hitCounter = 0;
+    this.healthPoints = 1;
+
     // the raid moves in a grid, the offset defines where the enemy is in the raid relative 
     // to the raid's x and y position
     this.offsetInRaidX = offsetInRaidX;
     this.offsetInRaidY = offsetInRaidY;
-
-    this.hitByAmmo = false;
   }
 
   render(context) {
@@ -22,13 +24,31 @@ class Enemy {
     this.enemyPosX = raidPosX + this.offsetInRaidX;
     this.enemyPosY = raidPosY + this.offsetInRaidY;
 
-    // check for collisions
-    this.stateManager.availableAmmoPool.forEach(ammo => {
-      if (!ammo.free && this.stateManager.checkForAmmoHit(ammo, this)) {
-        this.hitByAmmo = true;
-        ammo.reset();
+    // check if an enemy has crossed the lower bounds of the game
+    // if yes, finish the game
+    if (this.enemyPosY + this.stateManager.enemySize > this.stateManager.height) {
+      this.stateManager.gameOver = true;
+    }
+
+    // if the game is still going, check for enemy collisions
+    // with player and active ammunitions
+    if (!this.stateManager.gameOver) {
+      // check for collisions
+      this.stateManager.availableAmmoPool.forEach(ammo => {
+        if (!ammo.free && this.stateManager.checkEnemyCollision(this, ammo)) {
+          this.hitCounter++;
+          ammo.reset();
+          if (!this.stateManager.gameOver)
+            this.stateManager.score++;
+        }
+      })
+
+      // check if enemy hit the player
+      if (this.stateManager.checkEnemyCollision(this, this.stateManager.player)) {
+        this.stateManager.playerLives--;
+        this.hitCounter++;
       }
-    })
+    }
   }
 }
 
